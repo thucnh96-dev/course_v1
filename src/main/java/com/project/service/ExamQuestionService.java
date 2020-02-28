30dbf6fc1e50210631d70a87157df69f5bb8cfe2
TRUNGTHUC <thucnhpd02000@enjoyworks.co.kr>
2020-02-28 11:34:46 +0700

    add service methods.

--- src/main/java/com/project/service/ExamQuestionService.java
+++ src/main/java/com/project/service/ExamQuestionService.java
@@ -2,8 +2,13 @@ package com.project.service;
 
 import com.project.entity.ExamQuestion;
 
+import java.util.List;
+
 public interface ExamQuestionService {
     ExamQuestion findById(Long id);
+
     ExamQuestion save(ExamQuestion examQuestion);
+
+    List<ExamQuestion> saveAll(List<ExamQuestion> examQuestions);
 
 }
--- src/main/java/com/project/service/ExamQuestionServiceImpl.java
+++ src/main/java/com/project/service/ExamQuestionServiceImpl.java
@@ -5,11 +5,15 @@ import com.project.repository.ExamQuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ExamQuestionServiceImpl implements ExamQuestionService{
 
     @Autowired
     private ExamQuestionRepository examQuestionRepository;
+
+
 
     @Override
     public ExamQuestion findById(Long id) {
@@ -19,5 +23,10 @@ public class ExamQuestionServiceImpl implements ExamQuestionService{
     @Override
     public ExamQuestion save(ExamQuestion examQuestion) {
         return examQuestionRepository.save(examQuestion);
+    }
+
+    @Override
+    public List<ExamQuestion> saveAll(List<ExamQuestion> examQuestions) {
+        return examQuestionRepository.saveAll(examQuestions);
     }
 }
